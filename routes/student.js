4f9367e58456611d6c58683177ff1b676b63effc
riyaz shaikh <riyaz.s@osplabs.com>
2023-09-05 15:59:59 +0530

    added normal route

--- routes/student.js
+++ routes/student.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 
-const { Signup, Signin } = require("../controllers/student");
+const { Signup, Signin, Student } = require("../controllers/student");
 
 const { ValidateSignupFields, ValidateSigninFields, ValidateErrors } = require("../middlewares/validator");
 const { ValidateCityIdAndStateId, ValidateUserData, ValidateUserExists } = require("../middlewares/auth");
 
 router.post("/signup", ValidateSignupFields, ValidateErrors, ValidateUserExists, ValidateCityIdAndStateId, Signup);
 router.post("/signin", ValidateSigninFields, ValidateErrors, ValidateUserData, Signin);
+
+router.get('/', Student);
 
 module.exports = router;
